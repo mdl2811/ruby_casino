315fad80995a146a7f5a26e67225cf89e69f8640
Catherine Callow-Heusser <ccallowheusser@gmail.com>
2017-05-23 15:36:18 -0600

    pause while shuffling

--- Deck.rb
+++ Deck.rb
@@ -11,8 +11,10 @@ class Deck
     end
   end
   def shuffle!
+    puts "Wait just a sec while the deck is shuffled!"
     sound = Sounder::Sound.new "./media/262812__win88emily__card-shuffle-win88.mp3"
     sound.play
+    sleep(4)
     @cards.shuffle!
   end
   def draw
