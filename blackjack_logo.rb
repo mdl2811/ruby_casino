df7a193eb202b87a5ab74b4e6552c66631b4abe1
Keveren Freeman <freemankeveren@gmail.com>
2017-05-23 16:17:27 -0600

    b logo update

--- blackjack_logo.rb
+++ blackjack_logo.rb
@@ -9,7 +9,7 @@ lines [3] = "  88aaaaaa8P'  88  ,adPPYYba,   ,adPPYba,  88   ,d8   88  ,adPPYYba
 lines [4] = "  88""""""8b,  88  ''     `Y8  a8'     ''  88 ,a8'    88  ''     `Y8  a8'     ''  88 ,a8'    88"
 lines [5] = "  88      `8b  88  ,adPPPPP88  8b          8888[      88  ,adPPPPP88  8b          8888[      """
 lines [6] = "  88      a8P  88  88,    ,88  '8a,   ,aa  88`'Yba,   88  88,    ,88  '8a,   ,aa  88`'Yba,   aa"
-lines [7] = "  88888888P'   88  `'8bbdP'Y8   `'Ybbd8'  88   `Y8a  88  `'8bbdP'Y8   `'Ybbd8''  88   `Y8a  88"
+lines [7] = "  88888888P'   88  `'8bbdP'Y8   `'Ybbd8'  88   `Y8a  88  `'8bbdP'Y8    `'Ybbd8''  88   `Y8a  88"
 lines [8] = "                                                   ,88"
 lines [9] = "                                                 888P"
 
