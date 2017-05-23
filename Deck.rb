06389367c835ccae6b45df9aefbcace1780c4107
Michael LaBahn <mdlabahn@yahoo.com>
2017-05-23 12:49:05 -0600

    editss

--- Deck.rb
+++ Deck.rb
@@ -1,5 +1,3 @@
-Suits = %w[ Clubs Hearts Spades Diamonds ]
-Ranks = %w[ 1 2 3 4 5 6 7 8 9 10 11 12 13 ]
 @d = []
 
 class Deck
--- black_jack.rb
+++ black_jack.rb
@@ -22,7 +22,7 @@ class BlackJackHand
       @hand_value == 0 ? @hand_value = card.value.to_i : @hand_value += card.value.to_i
       @hand << card
     end
-    @hand
+    @hand_value
 
   end
 
@@ -31,19 +31,19 @@ class BlackJackHand
     puts "Player's hand:"
     puts player_cards.to_s
     dealer_cards = deal
-binding.pry
-    if player_cards.hand_value == 21
+
+    if player_cards == 21
       puts "Player wins with BlackJack!"
       puts "Dealer's hand:"
       puts dealer_cards.to_s
     else
-      until player_cards.hand_value > 21
-        play.switch_ace
+      until player_cards > 21
+        switch_ace
         puts "Do you want to hit(h) or stay(s)?"
         action = $stdin.gets.chomp
         if action == "h"
           puts "You chose to hit, here's your new hand"
-          play.hit
+          hit
           puts player_cards.to_s
         else
           puts "You chose to stay, here's your final hand"
@@ -52,18 +52,19 @@ binding.pry
         end
       end
 
+      dealer_cards = deal
       puts "Dealer's hand:"
       puts dealer_cards.to_s
 
-      while deal.hand_value < 17
+      while dealer_cards < 17
         dealer_cards.hit
         puts dealer_cards.to_s
       end
 
-      if player_cards.hand_value <= 21
-        if player_cards.hand_value > deal.hand_value
+      if player_cards <= 21
+        if player_cards > dealer_cards
           puts "Player wins"
-        elsif player_cards.hand_value == dealer_cards.hand_value
+        elsif player_cards == dealer_cards
           puts "Dealer and Player tied"
         else
           puts "Dealer wins"
@@ -72,7 +73,6 @@ binding.pry
         puts "Player busts. Dealer wins"
       end
     end
-
     puts "There are #{@d.remaining} cards in the deck"
   end
 
@@ -83,6 +83,7 @@ binding.pry
   end
 
   def switch_ace
+    binding.pry
     while @hand_value > 21 && @ace_count > 0
       @ace_count -= 1
       @hand_value -= 10
